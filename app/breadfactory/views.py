d8d8b2a584dcec0c9113f311dd2dc79dd1b7ad7b
EldarKlyuev <e.s.klyuev@gmail.com>
2024-05-20 01:48:24 +0300

    Add endpoint POST item

--- app/breadfactory/views.py
+++ app/breadfactory/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import (
+    OrderSerializer,
+    OrdererSerializer,
+    ItemSerializer,
+    FactorSerializer,
+    ProductionSerializer
+)
+
+
+class ItemApiView(APIView):
+    def post(self, request):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
