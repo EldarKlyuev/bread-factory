71ef66b1f088ea3b6bfa0d15056399c2c536804a
EldarKlyuev <e.s.klyuev@gmail.com>
2024-05-23 02:11:11 +0300

    Add GET requests

--- app/breadfactory/views.py
+++ app/breadfactory/views.py
@@ -10,8 +10,24 @@ from .serializers import (
     ProductionSerializer
 )
 
+from .models import (
+    Orders,
+    Factors,
+    Orderers,
+    Production,
+    Items
+)
+
 
 class ItemApiView(APIView):
+    def get(self, request):
+        try:
+            model = Items.objects.all()
+        except Exception:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ItemSerializer(model, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request):
         serializer = ItemSerializer(data=request.data)
         if serializer.is_valid():
@@ -20,6 +36,14 @@ class ItemApiView(APIView):
 
 
 class OrderApiView(APIView):
+    def get(self, request):
+        try:
+            model = Orders.objects.all()
+        except Exception:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = OrderSerializer(model, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request):
         serializer = OrderSerializer(data=request.data)
         if serializer.is_valid():
@@ -28,14 +52,32 @@ class OrderApiView(APIView):
 
 
 class FactorApiView(APIView):
+    def get(self, request):
+        try:
+            model = Factors.objects.all()
+        except Exception:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = FactorSerializer(model, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request):
         serializer = FactorSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ProductionApiView(APIView):
+    def get(self, request):
+        try:
+            model = Production.objects.all()
+        except Exception:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ProductionSerializer(model, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request):
         serializer = ProductionSerializer(data=request.data)
         if serializer.is_valid():
@@ -44,6 +86,14 @@ class ProductionApiView(APIView):
 
 
 class OrdererApiView(APIView):
+    def get(self, request):
+        try:
+            model = Orderers.objects.all()
+        except Exception:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = OrdererSerializer(model, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request):
         serializer = OrdererSerializer(data=request.data)
         if serializer.is_valid():
